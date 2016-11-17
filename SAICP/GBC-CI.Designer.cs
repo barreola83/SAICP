382780862de23e94f17960cac41e7a6ce35f8f90
Bryan Arreola <barreola83@gmail.com>
2016-11-16 21:37:32 -0600

    Se terminó de realizar el formulario para la consulta de ingresos.

--- SAICP/GBC-CI.Designer.cs
+++ SAICP/GBC-CI.Designer.cs
@@ -29,6 +29,7 @@ namespace SAICP
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.statusBar = new DevComponents.DotNetBar.Metro.MetroStatusBar();
             this.lblMessage = new DevComponents.DotNetBar.LabelItem();
             this.progressBarItem1 = new DevComponents.DotNetBar.ProgressBarItem();
@@ -36,6 +37,14 @@ namespace SAICP
             this.lblDate = new DevComponents.DotNetBar.LabelItem();
             this.lblHour = new DevComponents.DotNetBar.LabelItem();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.cldDate = new DevComponents.Editors.DateTimeAdv.MonthCalendarAdv();
+            this.lblSelectDate = new DevComponents.DotNetBar.LabelX();
+            this.dgvData = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.btnCancel = new DevComponents.DotNetBar.ButtonX();
+            this.dgvDateNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
             this.SuspendLayout();
             // 
             // statusBar
@@ -54,9 +63,9 @@ namespace SAICP
             this.lblBlank,
             this.lblDate,
             this.lblHour});
-            this.statusBar.Location = new System.Drawing.Point(0, 374);
+            this.statusBar.Location = new System.Drawing.Point(0, 233);
             this.statusBar.Name = "statusBar";
-            this.statusBar.Size = new System.Drawing.Size(625, 22);
+            this.statusBar.Size = new System.Drawing.Size(554, 22);
             this.statusBar.TabIndex = 0;
             // 
             // lblMessage
@@ -96,11 +105,108 @@ namespace SAICP
             this.timer.Enabled = true;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // cldDate
+            // 
+            this.cldDate.AutoSize = true;
+            // 
+            // 
+            // 
+            this.cldDate.BackgroundStyle.BackColor = System.Drawing.Color.LightBlue;
+            this.cldDate.BackgroundStyle.Class = "MonthCalendarAdv";
+            this.cldDate.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.cldDate.CommandsBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.cldDate.ContainerControlProcessDialogKey = true;
+            this.cldDate.DisplayMonth = new System.DateTime(2016, 11, 1, 0, 0, 0, 0);
+            this.cldDate.Location = new System.Drawing.Point(12, 45);
+            this.cldDate.Name = "cldDate";
+            // 
+            // 
+            // 
+            this.cldDate.NavigationBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground2;
+            this.cldDate.NavigationBackgroundStyle.BackColorGradientAngle = 90;
+            this.cldDate.NavigationBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.cldDate.NavigationBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.cldDate.Size = new System.Drawing.Size(170, 131);
+            this.cldDate.TabIndex = 1;
+            this.cldDate.Text = "monthCalendarAdv1";
+            // 
+            // lblSelectDate
+            // 
+            // 
+            // 
+            // 
+            this.lblSelectDate.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lblSelectDate.Location = new System.Drawing.Point(12, 16);
+            this.lblSelectDate.Name = "lblSelectDate";
+            this.lblSelectDate.Size = new System.Drawing.Size(128, 23);
+            this.lblSelectDate.TabIndex = 2;
+            this.lblSelectDate.Text = "Seleccione una fecha...";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvDateNumber,
+            this.dgvPrice,
+            this.dgvDate});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvData.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvData.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.dgvData.Location = new System.Drawing.Point(203, 16);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.Size = new System.Drawing.Size(343, 211);
+            this.dgvData.TabIndex = 3;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnCancel.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnCancel.Location = new System.Drawing.Point(12, 204);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancelar";
+            // 
+            // dgvDateNumber
+            // 
+            this.dgvDateNumber.HeaderText = "No. de cita";
+            this.dgvDateNumber.Name = "dgvDateNumber";
+            this.dgvDateNumber.ReadOnly = true;
+            // 
+            // dgvPrice
+            // 
+            this.dgvPrice.HeaderText = "Importe";
+            this.dgvPrice.Name = "dgvPrice";
+            this.dgvPrice.ReadOnly = true;
+            // 
+            // dgvDate
+            // 
+            this.dgvDate.HeaderText = "Fecha de cita";
+            this.dgvDate.Name = "dgvDate";
+            this.dgvDate.ReadOnly = true;
+            // 
             // frmQueryEarningRecords
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 396);
+            this.ClientSize = new System.Drawing.Size(554, 255);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.lblSelectDate);
+            this.Controls.Add(this.cldDate);
             this.Controls.Add(this.statusBar);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -109,7 +215,9 @@ namespace SAICP
             this.TitleText = "Consulta de ingresos por concepto de citas";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmQueryEarningRecords_FormClosed);
             this.Load += new System.EventHandler(this.frmQueryEarningRecords_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -122,5 +230,12 @@ namespace SAICP
         private DevComponents.DotNetBar.LabelItem lblDate;
         private DevComponents.DotNetBar.LabelItem lblHour;
         private System.Windows.Forms.Timer timer;
+        private DevComponents.Editors.DateTimeAdv.MonthCalendarAdv cldDate;
+        private DevComponents.DotNetBar.LabelX lblSelectDate;
+        private DevComponents.DotNetBar.Controls.DataGridViewX dgvData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvDateNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvDate;
+        private DevComponents.DotNetBar.ButtonX btnCancel;
     }
 }
